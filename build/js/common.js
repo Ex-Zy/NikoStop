3cef77f1b36edfa37b07a1b673a9569c864a8ecb
Ostapenko Inga <novosad88inga@gmail.com>
2017-07-05 11:17:33 +0300

    local fonts

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
+ $(".regular").slick({
+        dots: true,        
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: true,
+  		autoplaySpeed: 3000,
+      infinite: true,
+      responsive: [
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,       
+        dots: true
+      }
+    }]
+      });   
 
-	// go js
-
+ $(window).scroll(function(){
+      if ($(this).scrollTop() > $('.sect2').offset().top) {
+          $('.navbar').addClass('fixed');
+      } else {
+          $('.navbar').removeClass('fixed');
+      }
+});        
 });
