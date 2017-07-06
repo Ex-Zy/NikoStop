d359fc9fa2b30464480ef20f99725065d9be8fb7
Ostapenko Inga <novosad88inga@gmail.com>
2017-07-06 10:34:30 +0300

    add desctop version

--- build/js/app.js
+++ build/js/app.js
@@ -49,3 +49,34 @@ $(document).ready(function() {
       }
 });        
 });
+
+if(document.documentMode) {
+  $(".graf").addClass("ie-detected");
+}
+
+function getScrolled(Objs) {
+  var objsLength = Objs.length, i;
+  for (var i = 0; i < objsLength; i++) {
+    if(Objs[i].scrollTop > 0) {
+      return Objs[i].scrollTop;
+    }
+    continue;
+  }
+  return 0;
+}
+
+$(window).on("scroll", function(){
+  var scrolledTop = getScrolled(document.querySelectorAll("html, body")),
+      scrolledBottom = scrolledTop + window.innerHeight,
+
+      $elem = $(".graf"),
+      $elInfo = $elem.offset();
+      $elemTop = $elInfo.top,
+      $elemBottom = $elemTop + $elem.innerHeight();
+
+  if(scrolledBottom >= $elemTop && scrolledTop <= $elemBottom) {
+    $elem.addClass("is-animated");
+  } else {
+    $elem.removeClass("is-animated");
+  }
+});
