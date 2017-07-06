4e11caf19951076b15891bce0997a62a88741c1e
Ex-Zy <serega1905@ukr.net>
2017-07-06 17:45:27 +0300

    'parallax'

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,3 @@
 //=include lib/jquery.js
+//=include lib/parallax.js
 //=include common.js
--- src/js/common.js
+++ src/js/common.js
@@ -1,30 +1,58 @@
 $(document).ready(function() {
 	'use strict';
 
-	var WIN_WIDTH = $(window).width();
-	var WIN_HEIGHT = $(window).height();
+	// var WIN_WIDTH = $(window).width();
+	// var WIN_HEIGHT = $(window).height();
 
-	function showParallax(moveForce1, moveForce2) {
-		$('.js-parallax-section').mousemove(function(e) {
+	// function showParallax(moveForce) {
+	// 	$('.js-parallax-section').mousemove(function(e) {
 			
-			var moveX1 = (e.pageX - WIN_WIDTH/2) / (WIN_WIDTH/2) * -moveForce1;
-			var moveY1 = (e.pageY - WIN_HEIGHT/2) / (WIN_HEIGHT/2) * -moveForce1;
+	// 		var moveX = (e.pageX - WIN_WIDTH/2) / (WIN_WIDTH/2) * -moveForce;
+	// 		var moveY = (e.pageY - WIN_HEIGHT/2) / (WIN_HEIGHT/2) * -moveForce;
 
-			var moveX2 = (e.pageX - WIN_WIDTH/2) / (WIN_WIDTH/2) * -moveForce2;
-			var moveY2 = (e.pageY - WIN_HEIGHT/2) / (WIN_HEIGHT/2) * -moveForce2;
 
-			$('.js-parallax-1').css({
-				'right': moveX1 +'px',
-				'bottom': moveY1+'px'
-			});
+	// 		$('.js-parallax-1').css({
+	// 			'right': (moveX * 6) +'px',
+	// 			'bottom': (moveY * 6)+'px'
+	// 		});
 
-			$('.js-parallax-2').css({
-				'left': (moveX2 * 7)+'px',
-				'top': (moveY2 * 7)+'px',
-			});
+	// 		$('.js-parallax-2').css({
+	// 			'left': (moveX * 20) +'px',
+	// 			'top': (moveY * 20)+'px'
+	// 		});
 
-		});
-	}
-	showParallax(10, 2);
+	// 		$('.js-parallax-3').css({
+	// 			'right': (moveX * 15) +'px',
+	// 			'bottom': (moveY * 15)+'px'
+	// 		});
+
+	// 		$('.js-parallax-4').css({
+	// 			'left': (moveX * 12) +'px',
+	// 			'top': (moveY * 12)+'px'
+	// 		});
+
+	// 		$('.js-parallax-5').css({
+	// 			'left': (moveX * 25) +'px',
+	// 			'top': (moveY * 25)+'px'
+	// 		});
+
+	// 	});
+	// }
+	// showParallax(2);
+	// $('.js-parallax-1').jKit('parallax', { 'strength': '1', 'axis': 'both'});
+	// $('.js-parallax-2').jKit('parallax', { 'strength': '2', 'axis': 'both'});
+	// $('.js-parallax-2').jKit('parallax', { 'strength': '5', 'axis': 'y', 'detect': 'scroll' });
+	var scene1 = $('#scene-1').get(0);
+	var scene2 = $('#scene-2').get(0);
+	var scene3 = $('#scene-3').get(0);
+
+	var parallax1 = new Parallax(scene1, {
+		invertX: false,
+		invertY: false,
+	});
+	var parallax2 = new Parallax(scene2);
+	var parallax3 = new Parallax(scene3);
+
+
 
 });
